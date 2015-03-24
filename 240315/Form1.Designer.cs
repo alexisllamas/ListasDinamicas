0cfc2b638b9bd94d1a4cd67b4412838b103361f8
alexisllamas <alexisnavarro23@gmail.com>
2015-03-24 15:50:13 -0600

    It works!

--- 240315/Form1.Designer.cs
+++ 240315/Form1.Designer.cs
@@ -28,20 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.cmdAgregar = new System.Windows.Forms.Button();
+            this.cmdMostrar = new System.Windows.Forms.Button();
+            this.txtDatos = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // cmdAgregar
+            // 
+            this.cmdAgregar.Location = new System.Drawing.Point(29, 44);
+            this.cmdAgregar.Name = "cmdAgregar";
+            this.cmdAgregar.Size = new System.Drawing.Size(75, 23);
+            this.cmdAgregar.TabIndex = 0;
+            this.cmdAgregar.Text = "Agregar";
+            this.cmdAgregar.UseVisualStyleBackColor = true;
+            this.cmdAgregar.Click += new System.EventHandler(this.cmdAgregar_Click);
+            // 
+            // cmdMostrar
+            // 
+            this.cmdMostrar.Location = new System.Drawing.Point(45, 132);
+            this.cmdMostrar.Name = "cmdMostrar";
+            this.cmdMostrar.Size = new System.Drawing.Size(75, 23);
+            this.cmdMostrar.TabIndex = 1;
+            this.cmdMostrar.Text = "Mostrar";
+            this.cmdMostrar.UseVisualStyleBackColor = true;
+            this.cmdMostrar.Click += new System.EventHandler(this.cmdMostrar_Click);
+            // 
+            // txtDatos
+            // 
+            this.txtDatos.Location = new System.Drawing.Point(45, 184);
+            this.txtDatos.Multiline = true;
+            this.txtDatos.Name = "txtDatos";
+            this.txtDatos.Size = new System.Drawing.Size(194, 99);
+            this.txtDatos.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(383, 310);
+            this.Controls.Add(this.txtDatos);
+            this.Controls.Add(this.cmdMostrar);
+            this.Controls.Add(this.cmdAgregar);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button cmdAgregar;
+        private System.Windows.Forms.Button cmdMostrar;
+        private System.Windows.Forms.TextBox txtDatos;
     }
 }
 
--- 240315/Form1.cs
+++ 240315/Form1.cs
@@ -12,9 +12,56 @@ namespace _240315
 {
     public partial class Form1 : Form
     {
+        Random gen = new Random();
+        Nodo inicio;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void cmdAgregar_Click(object sender, EventArgs e)
+        {
+            Nodo nuevo = new Nodo(gen.Next(100));
+            agregar(nuevo);
+        }
+
+        private void agregar(Nodo nuevo)
+        {
+            if (inicio == null)
+                inicio = nuevo;
+            else
+                agregar(inicio, nuevo);
+        }
+         
+        private void agregar(Nodo ultimo, Nodo nuevo)
+        {
+            if (ultimo.Siguiente == null)
+                ultimo.Siguiente = nuevo;
+            else
+                agregar(ultimo.Siguiente, nuevo);
+        }
+
+        private void cmdMostrar_Click(object sender, EventArgs e)
+        {
+            txtDatos.Text = mostrar();
+        }
+
+        private string mostrar()
+        {
+            string cadenita = "";
+            if(inicio != null)
+                cadenita = mostrar(inicio);
+            return cadenita;
+        }
+
+        private string mostrar(Nodo nodo)
+        {
+            string cadenita = "";
+            if (nodo.Siguiente == null)
+                cadenita += nodo.ToString();
+            else
+                cadenita += nodo.ToString() + Environment.NewLine + mostrar(nodo.Siguiente);
+            return cadenita;
         }
     }
 }

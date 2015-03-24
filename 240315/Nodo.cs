4264e3ef5a6e08e2d8545d5e0e9ebe6484edb9e9
alexisllamas <alexisnavarro23@gmail.com>
2015-03-24 15:08:01 -0600

    First commit

---
+++ 240315/Nodo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _240315
+{
+    class Nodo
+    {
+        private int _dato;
+
+        public int Dato
+        {
+            get { return _dato; }
+            set { _dato = value; }
+        }
+        private Nodo _siguiente;
+
+        public Nodo Siguiente
+        {
+            get { return _siguiente; }
+            set { _siguiente = value; }
+        }
+
+        public Nodo(int dato)
+        {
+            _dato = dato;
+        }
+
+        public override string ToString()
+        {
+            return _dato.ToString();
+        }
+    }
+}
